6ed3e57282892276b1ee3d5ee837b23eb5ea6be5
Kim Wittenburg <codello@wittenburg.kim>
2022-08-17 14:28:10 +0200

    Prettify texts

--- docassemble/juja/data/templates/Benachrichtigung.md
+++
@@ -1,9 +0,0 @@
-Hallo,
-
-Das Ensemble ${ ensemble.name } wurde soeben zu Jugend Jazzt angemeldet.
-
-[Anmeldeliste öffnen](${ tabelle['datei']['webUrl'] })  
-[Anmeldeordner öffnen](${ ordner['webUrl'] })
-
-Viele Grüße  
-Das Anmeldungssystem
--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude,
 
 setup(
     name='docassemble.juja',
-    version='0.1.0',
+    version='0.1.1',
     description=__doc__,
     long_description=long_description(),
     long_description_content_type='text/markdown',
