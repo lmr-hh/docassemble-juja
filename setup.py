fee7b438dc9c1a45952fe01ea038861f2228f8a2
Kim Wittenburg <codello@wittenburg.kim>
2022-09-25 22:31:10 +0200

    Fix asserts

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude,
 
 setup(
     name='docassemble.juja',
-    version='0.2.3',
+    version='0.2.4',
     description=__doc__,
     long_description=long_description(),
     long_description_content_type='text/markdown',
